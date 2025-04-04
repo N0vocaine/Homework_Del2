2e601a10849ebd13278c7b9062575f95253ced0a
adria <adrianam.bucur@gmail.com>
2025-04-04 11:03:17 +0200

    Update

--- src/test/java/stepdefinitions/RegistrationStepdefs.java
+++ src/test/java/stepdefinitions/RegistrationStepdefs.java
@@ -1,4 +1,84 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RegistrationStepdefs {
+    WebDriver driver;
+
+    @Given("I navigate to the Basketball England registration page")
+    public void iNavigateToTheBasketballEnglandRegistrationPage() {
+        driver = new ChromeDriver();
+        driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
+        driver.manage().window().maximize();
+    }
+
+    @When("I enter valid member details")
+    public void iEnterValidMemberDetails() {
+        driver.findElement(By.cssSelector("#dp")).sendKeys("14-03-1980"); // Date of birth
+        driver.findElement(By.cssSelector("#member_firstname")).sendKeys("Johanna"); // First name
+        driver.findElement(By.cssSelector("#member_lastname")).sendKeys("Smith"); // Last name
+        driver.findElement(By.cssSelector("#member_emailaddress")).sendKeys("johanna@gmail.com"); // Email adress
+        driver.findElement(By.cssSelector("#member_confirmemailaddress")).sendKeys("johanna@gmail.com"); //Confirm email adress
+    }
+
+    @And("I enter matching passwords")
+    public void iEnterMatchingPasswords() {
+        driver.findElement(By.cssSelector("#signupunlicenced_password")).sendKeys("abc123"); //Password
+        driver.findElement(By.cssSelector("#signupunlicenced_confirmpassword")).sendKeys("abc123");  // Confirm password
+    }
+
+    @And("I accept the terms and conditions")
+    public void iAcceptTheTermsAndConditions() {
+        driver.findElement(By.cssSelector("#signup_form > div:nth-child(11) > div > div > div:nth-child(12) > " +
+                "div > label > span.box")).click();  //Select Fan box
+        driver.findElement(By.cssSelector("#signup_form > div:nth-child(12) > div > div:nth-child(2) > " +
+                "div:nth-child(1) > label > span.box")).click(); // Accept terms and conditions
+        driver.findElement(By.cssSelector("#signup_form > div:nth-child(12) > div > div:nth-child(2) > " +
+                "div.md-checkbox.margin-top-10 > label > span.box")).click(); // Accept "I am over 18"
+        driver.findElement(By.cssSelector("#signup_form > div:nth-child(12) > div > div:nth-child(4) >" +
+                " label > span.box")).click();  // Receive marketing communications
+        driver.findElement(By.cssSelector("#signup_form > div:nth-child(12) > div > div:nth-child(7) > " +
+                "label > span.box")).click();  // Accept Code of Ethics
+    }
+
+    @And("I submit the registration form")
+    public void iSubmitTheRegistrationForm() {
+        driver.findElement(By.cssSelector("#signup_form > div.form-actions.noborder > input")).click();  // Confirm and join
+    }
+
+    @Then("I should see a confirmation that the account was created")
+    public void iShouldSeeAConfirmationThatTheAccountWasCreated() {
+        driver.findElement(By.cssSelector("body > div > div.page-content-wrapper > div > h2"))
+                .sendKeys("THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND");
+    }
+
+    @When("I enter member details without a last name")
+    public void iEnterMemberDetailsWithoutALastName() {
+    }
+
+    @Then("I should see an error message for the missing last name")
+    public void iShouldSeeAnErrorMessageForTheMissingLastName() {
+    }
+
+    @And("I enter non-matching passwords")
+    public void iEnterNonMatchingPasswords() {
+    }
+
+    @Then("I should see an error message for password mismatch")
+    public void iShouldSeeAnErrorMessageForPasswordMismatch() {
+    }
+
+    @And("I do not accept the terms and conditions")
+    public void iDoNotAcceptTheTermsAndConditions() {
+    }
+
+    @Then("I should see an error message for unaccepted terms")
+    public void iShouldSeeAnErrorMessageForUnacceptedTerms() {
+    }
 }
---
+++ src/test/resources/features/RegisterUser.feature
@@ -0,0 +1,34 @@
+Feature: User Registration on Basketball England Website
+  This feature tests the registration form functionality for creating a new supporter account.
+
+  Scenario: Successful user registration
+    Given I navigate to the Basketball England registration page
+    When I enter valid member details
+    And I enter matching passwords
+    And I accept the terms and conditions
+    And I submit the registration form
+    Then I should see a confirmation that the account was created
+
+  Scenario: User registration with missing last name
+    Given I navigate to the Basketball England registration page
+    When I enter member details without a last name
+    And I enter matching passwords
+    And I accept the terms and conditions
+    And I submit the registration form
+    Then I should see an error message for the missing last name
+
+  Scenario: User registration with non-matching passwords
+    Given I navigate to the Basketball England registration page
+    When I enter valid member details
+    And I enter non-matching passwords
+    And I accept the terms and conditions
+    And I submit the registration form
+    Then I should see an error message for password mismatch
+
+  Scenario: User registration without accepting terms and conditions
+    Given I navigate to the Basketball England registration page
+    When I enter valid member details
+    And I enter matching passwords
+    And I do not accept the terms and conditions
+    And I submit the registration form
+    Then I should see an error message for unaccepted terms
