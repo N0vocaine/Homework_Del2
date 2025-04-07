ab0a2e090259bd971dca09fc73aa52ea55198c9d
adria <adrianam.bucur@gmail.com>
2025-04-07 14:37:25 +0200

    Final

--- src/test/java/stepdefinitions/RegistrationStepdefs.java
+++ src/test/java/stepdefinitions/RegistrationStepdefs.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.UUID;
+
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
