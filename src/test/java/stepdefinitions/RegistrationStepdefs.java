54d564b5d26c2a62b601863129cd53c9b80dbbf5
adria <adrianam.bucur@gmail.com>
2025-04-09 10:24:13 +0200

    Final

--- src/test/java/stepdefinitions/RegistrationStepdefs.java
+++ src/test/java/stepdefinitions/RegistrationStepdefs.java
@@ -19,7 +19,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RegistrationStepdefs {
     WebDriver driver;
-    //String uniqecode = String.valueOf(UUID.randomUUID());
     String email = "lara+" + System.currentTimeMillis() + "@gmail.com";
 
     private void waitAndClick(String css) {
@@ -32,7 +31,7 @@ public class RegistrationStepdefs {
     public void iNavigateToTheBasketballEnglandRegistrationPage(String browser) {
         if (browser.equals("chrome")) driver = new ChromeDriver();
         if (browser.equals("edge")) driver = new EdgeDriver();
-        //driver = new ChromeDriver();
+
         driver.manage().window().maximize();
         driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
 
@@ -49,8 +48,10 @@ public class RegistrationStepdefs {
 
     @And("I enter matching passwords")
     public void iEnterMatchingPasswords() {
-        driver.findElement(By.cssSelector("#signupunlicenced_password")).sendKeys("abc123"); //Password
-        driver.findElement(By.cssSelector("#signupunlicenced_confirmpassword")).sendKeys("abc123");  // Confirm password
+        driver.findElement(By.cssSelector("#signupunlicenced_password"))
+                .sendKeys("abc123"); //Password
+        driver.findElement(By.cssSelector("#signupunlicenced_confirmpassword"))
+                .sendKeys("abc123");  // Confirm password
     }
 
     @And("I accept the terms and conditions")
@@ -76,15 +77,15 @@ public class RegistrationStepdefs {
     public void iSubmitTheRegistrationForm() {
         waitAndClick("#signup_form > div.form-actions.noborder > input");   // Confirm and join
 
-
     }
 
     @And("I should see a success message")
     public void iShouldSeeASuccessMessage() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement successMessage = wait.until(ExpectedConditions.elementToBeClickable(By
-                .cssSelector("body > div > div.page-content-wrapper > div > h2")));
+                .cssSelector("body > div > div.page-content-wrapper > div > div > h2")));
         assertTrue(successMessage.isDisplayed());
+        System.out.println("Your Membership Number is: " + successMessage.getText());
     }
 
 
@@ -92,63 +93,56 @@ public class RegistrationStepdefs {
     public void iEnterMemberDetailsWithoutALastName() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        driver.findElement(By.cssSelector("#dp")).sendKeys("14-03-1980");// Date of birth
+        driver.findElement(By.cssSelector("#dp")).sendKeys("14-03-1981");// Date of birth
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#dp")));
-
-        driver.findElement(By.cssSelector("#member_firstname")).sendKeys("Johanna"); // First name
-        // driver.findElement(By.cssSelector("#member_lastname")).sendKeys(" "); // Last name
-        driver.findElement(By.cssSelector("#member_emailaddress")).sendKeys("johanna@gmail.com"); // Email adress
-        driver.findElement(By.cssSelector("#member_confirmemailaddress")).sendKeys("johanna@gmail.com"); //Confirm email adress
-        driver.findElement(By.cssSelector("#signupunlicenced_password")).sendKeys("def456"); // Password
-        driver.findElement(By.cssSelector("#signupunlicenced_confirmpassword")).sendKeys("def456");  //Confirm password
+        driver.findElement(By.cssSelector("#member_firstname"))
+                .sendKeys("Lara"); // First name
+        driver.findElement(By.cssSelector("#member_emailaddress")).sendKeys(email); // Email address
+        driver.findElement(By.cssSelector("#member_confirmemailaddress")).sendKeys(email); //Confirm email address
         iAcceptTheTermsAndConditions();
-
 
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#signup_form > div.form-actions.noborder > input")));
         driver.findElement(By.cssSelector("#signup_form > div.form-actions.noborder > input")).click();
-
-
     }
 
     @Then("I should see an error message for the missing last name")
     public void iShouldSeeAnErrorMessageForTheMissingLastName() {
-        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
         WebElement error = driver.findElement(By.cssSelector("#signup_form > div:nth-child(6) > div:nth-child(2) > div > span > span"));
         assertTrue(error.isDisplayed(), "Last name is required");
-        //driver.quit();
+        System.out.println("Last name is required");
+
     }
 
     @And("I enter non-matching passwords")
     public void iEnterNonMatchingPasswords() {
-        driver.findElement(By.cssSelector("#signupunlicenced_password")).sendKeys("abc123");
-        driver.findElement(By.cssSelector("#signupunlicenced_confirmpassword")).sendKeys("def456"); // Wrong password
+        driver.findElement(By.cssSelector("#signupunlicenced_password"))
+                .sendKeys("abc123");
+        driver.findElement(By.cssSelector("#signupunlicenced_confirmpassword"))
+                .sendKeys("def456"); // Wrong password
         iAcceptTheTermsAndConditions();
     }
 
     @Then("I should see an error message for password mismatch")
     public void iShouldSeeAnErrorMessageForPasswordMismatch() {
-        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
         WebElement error = driver.findElement((By.cssSelector("#signup_form > div:nth-child(9) > div > div.row > div:nth-child(2) > div > span > span")));
         assertTrue(error.isDisplayed(), "Password did not match");
-        //driver.quit();
+        System.out.println("Password did not match");
     }
 
     @And("I do not accept the terms and conditions")
-    public void iDoNotAcceptTheTermsAndConditions() {
+    public void iDoNotAcceptTheTermsAndConditions() {    // Intentionally left empty
     }
 
     @Then("I should see an error message for unaccepted terms")
     public void iShouldSeeAnErrorMessageForUnacceptedTerms() {
-        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
         WebElement error = driver.findElement(By.cssSelector("#signup_form > div:nth-child(12) > " +
                 "div > div:nth-child(2) > div:nth-child(1) > span > span"));
         assertTrue(error.isDisplayed());
-        //driver.quit();
+        System.out.println("You must accept the terms and conditions!");
+
     }
-
-
 }
 
 
